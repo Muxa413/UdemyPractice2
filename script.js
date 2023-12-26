da07af3153adb4c20ce84c1a7c272c2bd8242842
Muxa413 <99648137+Muxa413@users.noreply.github.com>
2023-12-26 13:14:15 +0000

    My solution Practice 2

--- script.js
+++ script.js
@@ -15,3 +15,73 @@
 // Код возьмите из предыдущего домашнего задания
 
 'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+// #1
+for (let i = 0; i < 2; i++) {
+  const
+    a = prompt('Один из последних просмотренных фильмов?', ""),
+    b = prompt('На сколько оцените его?', "");
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+/*
+// #2
+let num = 0;
+while (num < 2) {
+  num++;
+  const
+    a = prompt('Один из последних просмотренных фильмов?', ""),
+    b = prompt('На сколько оцените его?', "");
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    console.log("error");
+    num--;
+  }
+};
+
+// #3
+let num = 0;
+do {
+  num++;
+  const
+    a = prompt('Один из последних просмотренных фильмов?', ""),
+    b = prompt('На сколько оцените его?', "");
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    console.log("error");
+    num--;
+  }
+} while (num < 2);
+*/
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов")
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель")
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман")
+} else {
+  console.log("Произошла ошибка")
+};
+
+
+console.log(personalMovieDB);
